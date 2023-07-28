0e033833a7c3534cbe71fadbd6119fdeb29485c2
Hellenwamaitha <135101770+Hellenwamaitha@users.noreply.github.com>
2023-07-28 17:29:46 +0300

    Update App.js

--- scr/components/App.js
+++ scr/components/App.js
@@ -1,16 +1,1 @@
-import React from "react";
-import BotCollection from './BotCollection'
 
-
-// the App component should render out the BotCollection component
-
-function App() {
-  return (
-    <div>
-     
-     <BotCollection />
-    </div>
-  );
-}
-
-export default App;
