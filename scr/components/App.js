7d3e6bc33e7b074c1a252a901fb7e51bf114e8b9
hellenwamaitha <hellenwama254@gmail.com>
2023-07-28 13:53:17 +0300

    React app that displays Bot

---
+++ scr/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import BotCollection from './BotCollection'
+
+
+// the App component should render out the BotCollection component
+
+function App() {
+  return (
+    <div>
+     
+     <BotCollection />
+    </div>
+  );
+}
+
+export default App;
